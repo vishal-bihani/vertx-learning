7856f7e041f3ed04176f7644b7e5a87f3a5f8a52
Vishal <vishal.bihani2@gmail.com>
2022-07-12 22:26:47 +0530

    Using context to run code on event loop with old threading model

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,15 +0,0 @@
-
-?
-settings.gradle,0/5/05efc8b1657769a27696d478ded1e95f38737233
-<
-build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
-X
-(src/main/java/org/example/VertxEcho.java,d/e/de68440247a0fbf0ce4da82611d45a5c95c13e1b
-\
-,src/main/java/org/example/HelloVerticle.java,1/7/17feaa592b1d091a17e30c98e47b2a6a75421f68
-\
-,src/main/java/org/example/EmptyVerticle.java,b/8/b81b3394c92da485ce9fb277bee616d4cd7c1b56
-[
-+src/main/java/org/example/SomeVerticle.java,7/f/7feeb6b07126769be8807e4c56a1b53a28943a85
-W
-'src/main/java/org/example/Deployer.java,f/8/f8feeab71354579bc05a5dab2628a9ef49d06fc1
---
+++ src/main/java/org/example/MixedThreading.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Context;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CountDownLatch;
+
+public class MixedThreading extends AbstractVerticle {
+    private static final Logger log = LoggerFactory.getLogger(MixedThreading.class);
+
+    @Override
+    public void start(Promise<Void> startPromise) throws Exception {
+        Context context = vertx.getOrCreateContext();
+
+        new Thread(() -> {
+            try {
+                run(context);
+                startPromise.complete();
+            } catch (InterruptedException e) {
+                log.error("Something went wrong! with message: {}", e.getMessage());
+            }
+        }).start();
+    }
+
+    public void run(Context context) throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        log.info("In a non Vert.x thread");
+
+        context.runOnContext(handler -> {
+            log.info("In the event loop");
+
+            vertx.setTimer(1000, timerHandler -> {
+                log.info("Final countdown");
+                latch.countDown();
+            });
+        });
+
+        log.info("Waiting on countdown latch");
+        latch.await();
+        log.info("Terminated");
+    }
+
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+        vertx.deployVerticle(new MixedThreading());
+    }
+}
